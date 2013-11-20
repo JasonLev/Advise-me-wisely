ffbdee1578f715fa6a228550ec1d350e02eea24b
JasonLev <jasond.levy@gmail.com>
2013-11-20 10:45:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvisemewiselyApp::Application.config.secret_token = 'f77013c07f12787f615462bb2075c70c7fb9dfe62bee2baca91d223ce94b52dfc390de28d174d40304c19748ec53e409c4c9a991b85757499bd8e6dc0a68ebd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdvisemewiselyApp::Application.config.session_store :cookie_store, key: '_advisemewisely_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdvisemewiselyApp::Application.config.session_store :active_record_store
